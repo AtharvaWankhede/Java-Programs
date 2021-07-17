0c952b1d787dd057ca7c902dd6c5da244190bdce
AtharvaWankhede <65616231+AtharvaWankhede@users.noreply.github.com>
2021-07-17 12:34:59 +0530

    Add files via upload

---
+++ Arraystart.java
@@ -0,0 +1,16 @@
+class ABC{
+	int arr[]= {3,4,5,321,78,655};
+	public ABC() {
+		for(int i=0;i<6;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
+public class Arraystart {
+	public static void main(String[]args) {
+		ABC obj=new ABC();
+		
+	}
+	
+
+}
---
+++ ReverseArray.java
@@ -0,0 +1,10 @@
+
+public class ReverseArray {
+	public static void main(String args[]) {
+	int a[]= {4,5,6,7,8,9,10};
+	for(int i=6;i>=0;i--) {
+		System.out.println(a[i]);
+		
+	}
+	}
+}
