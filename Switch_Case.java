eeb3a75ae7383cd0bc4f5116c8653bd798da0631
AtharvaWankhede <65616231+AtharvaWankhede@users.noreply.github.com>
2021-07-20 23:26:02 +0530

    Add files via upload

---
+++ Switch_Case.java
@@ -0,0 +1,27 @@
+
+public class Switch_Case {
+
+	public static void main(String[] args) {
+		int n=4;
+		switch(n) {
+		case 1:
+			System.out.println("One");
+			break;
+		case 2:
+			System.out.println("two");
+			break;
+		case 3:
+			System.out.println("three");
+			break;
+		case 4:
+			System.out.println("Four");
+			break;
+		case 5:
+			System.out.println("Five");
+			break;
+		default:
+			System.out.println("No matching case");
+		} 
+		
+	}
+}
